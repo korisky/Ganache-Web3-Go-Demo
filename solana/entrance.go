e22fa70808c625c90e82970410767a8231372aeb
roylic <roylic@wokoworks.com>
2023-03-07 17:38:13 +0800

    correct mod stuff for letting example running again

--- solana/entrance.go
+++ solana/entrance.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"github.com/portto/solana-go-sdk/client"
-	"github.com/portto/solana-go-sdk/common"
 	"github.com/portto/solana-go-sdk/rpc"
 	"net/http"
-	"web3Demo/solana/httpProxy"
+	"test/go/solana/balance"
+	"test/go/solana/httpProxy"
 )
 
 var (
@@ -24,29 +23,6 @@ var (
 
 // main are following stuff from https://yihau.gitbook.io/solana-go/tour/create-token-account/associated-token-account
 func main() {
-	//balance.TryGetAssociatedTokenAddressBalance(cli, address, tokenAddress)
-	associatedTokenAddress, _, _ := OwnFindAssociatedTokenAddress(address, tokenAddress)
-	fmt.Println("associated token address ", associatedTokenAddress)
-}
-
-// OwnFindAssociatedTokenAddress for Java migration use, but found out Java not support UnsignedBytes
-func OwnFindAssociatedTokenAddress(accountAddress, tokenAddress string) (common.PublicKey, uint8, error) {
-	walletAddress := common.PublicKeyFromString(accountAddress)
-	tokenMintAddress := common.PublicKeyFromString(tokenAddress)
-	fmt.Println("walletAddress bytes: ", walletAddress.Bytes())
-	fmt.Println(len(walletAddress.Bytes()))
-	fmt.Println("tokenProgram bytes: ", common.TokenProgramID.Bytes())
-	fmt.Println(len(common.TokenProgramID.Bytes()))
-	fmt.Println("tokenMintAddress bytes: ", tokenMintAddress.Bytes())
-	fmt.Println(len(tokenMintAddress.Bytes()))
-
-	seeds := [][]byte{}
-	seeds = append(seeds, walletAddress.Bytes())
-	seeds = append(seeds, common.TokenProgramID.Bytes())
-	seeds = append(seeds, tokenMintAddress.Bytes())
-
-	fmt.Println("whole seeds bytes: ", seeds)
-	fmt.Println(len(seeds))
-
-	return common.FindProgramAddress(seeds, common.SPLAssociatedTokenAccountProgramID)
+	balance.TryGetBalance(cli, address)
+	balance.TryGetAssociatedTokenAddressBalance(cli, address, tokenAddress)
 }
