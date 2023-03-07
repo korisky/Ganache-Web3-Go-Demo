1d270ab54981fd7fc0e61248f36f061f08bd4db2
roylic <roylic@wokoworks.com>
2023-03-07 19:00:19 +0800

    not working on token transfer

--- solana/transfer/transfer.go
+++ solana/transfer/transfer.go
@@ -7,7 +7,7 @@ import (
 	"github.com/portto/solana-go-sdk/program/sysprog"
 	"github.com/portto/solana-go-sdk/types"
 	"log"
-	"web3Demo/solana/accounts"
+	"test/go/solana/accounts"
 )
 
 // TryTransferSimple is for pure SOL transfer
@@ -30,7 +30,6 @@ func TryTransferSimple(cli *client.Client, base58priKey string) {
 			}),
 		},
 	})
-
 	// 3. create tx by msg + signer
 	tx, err := types.NewTransaction(types.NewTransactionParam{
 		Message: msg,
@@ -39,7 +38,6 @@ func TryTransferSimple(cli *client.Client, base58priKey string) {
 	if err != nil {
 		log.Fatalf("error: %v\n", err)
 	}
-
 	// 4. send txn
 	txHash, err := cli.SendTransaction(context.Background(), tx)
 	if err != nil {
@@ -48,3 +46,35 @@ func TryTransferSimple(cli *client.Client, base58priKey string) {
 
 	fmt.Printf("On-chain txHash: %v\n", txHash)
 }
+
+// TryTransferToken is for transfer token
+//func TryTransferToken(cli *client.Client, base58priKey, accountTokenAddress, tokenAddress string) {
+//	account, _ := types.AccountFromBase58(base58priKey)
+//	// 1. fetch recent block-hash
+//	resp, _ := cli.GetLatestBlockhash(context.Background())
+//	// 2. create token transfer msg
+//	instruction := token.TransferChecked(token.TransferCheckedParam{
+//		From:     common.PublicKeyFromString(accountTokenAddress),
+//		To:       common.PublicKeyFromString(accountTokenAddress),
+//		Mint:     common.PublicKeyFromString(tokenAddress),
+//		Auth:     account.PublicKey,
+//		Signers:  nil,
+//		Amount:   5000000,
+//		Decimals: 6,
+//	})
+//	// create a message
+//	message := types.NewMessage(types.NewMessageParam{
+//		FeePayer:        account.PublicKey,
+//		RecentBlockhash: resp.Blockhash, // recent blockhash
+//		Instructions: []types.Instruction{
+//			sysprog.Transfer(),
+//		},
+//	})
+//
+//
+//	// create tx by message + signer
+//	tx, err := types.NewTransaction(types.NewTransactionParam{
+//		Message: message,
+//		Signers: []types.Account{feePayer, alice},
+//	})
+//}
