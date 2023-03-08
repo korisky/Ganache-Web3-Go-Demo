ac548a3b2f91e6bca27a8f1058cf7a4b60ca5a11
roylic <lykismekk@outlook.com>
2023-03-08 22:40:58 +0800

    correct the module name

--- solana/entrance.go
+++ solana/entrance.go
@@ -4,8 +4,8 @@ import (
 	"github.com/portto/solana-go-sdk/client"
 	"github.com/portto/solana-go-sdk/rpc"
 	"net/http"
-	"test/go/solana/balance"
-	"test/go/solana/httpProxy"
+	"web3Demo/solana/balance"
+	"web3Demo/solana/httpProxy"
 )
 
 var (
