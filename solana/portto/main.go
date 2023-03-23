6eb6fe86b81c4314c25d0dac8cbf4a54466ea3ca
roylic <roylic@wokoworks.com>
2023-03-23 11:54:04 +0800

    tmp commit

--- solana/portto/main.go
+++ solana/portto/main.go
@@ -9,11 +9,12 @@ import (
 
 var (
 	// own accountAddress account
-	accountAddress         = "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D"
-	assTokenAccountAddress = "Gd8nxWzbnJ2zwtn5TukvEMKKjjbFhdtqA1L67DgnRvXc"
-	tokenMintAddress       = "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"
-	nftMintAddress         = "EZqtsCxYpYtNaX1Pd2ep3ZUVxS6qHLVQriugvbKGEahk"
-	ownEndpoint            = "https://solana-devnet.g.alchemy.com/v2/On35d8LdFc1QGYD-wCporecGj359qian"
+	accountAddress           = "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D"
+	assTokenAccountAddress   = "Gd8nxWzbnJ2zwtn5TukvEMKKjjbFhdtqA1L67DgnRvXc"
+	tokenMintAddress         = "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"
+	nftMintAddress           = "EZqtsCxYpYtNaX1Pd2ep3ZUVxS6qHLVQriugvbKGEahk"
+	nftCollectionMintAddress = "ARHE7qXefr79DqyApiEkZ2QwnyzfAnUew4jRXfkMBVT2"
+	ownEndpoint              = "https://solana-devnet.g.alchemy.com/v2/On35d8LdFc1QGYD-wCporecGj359qian"
 	// custom connection would be:
 	cli = client.New(rpc.WithEndpoint(ownEndpoint), rpc.WithHTTPClient(httpProxy.GetHttpClient()))
 )
@@ -21,13 +22,20 @@ var (
 // main are following stuff from https://portto.github.io/solana-go-sdk/tour/token-transfer.html
 func main() {
 
+	//accounts.TryFindAssociatedTokenAddress("GCTFHbBcLs3vsNkCKLz2uCq5boZhMHbuawuFuhbYttc8", tokenMintAddress)
+
+	//balance.TryGetBalance(cli, accountAddress)
+
 	//tokenBalance, u := balance.TryGetTokenBalance(cli, "Gd8nxWzbnJ2zwtn5TukvEMKKjjbFhdtqA1L67DgnRvXc")
 	//spew.Dump(tokenBalance, u)
 
+	// get all accounts by this owner
 	accounts.TryGetTokenAccountsByOwner(cli, accountAddress)
+	// decode nft mint account
+	//nft.TryDecodeNft(cli, nftMintAddress)
+	//nft.TryDecodeNft(cli, nftCollectionMintAddress)
 
 	//accounts.TryFindAssociatedTokenAddress(accountAddress, nftMintAddress)
-	//nft.TryDecodeNft(cli, nftMintAddress)
 
 	//transfer.TryTransferToken(cli, "", assTokenAccountAddress, tokenMintAddress)
 
