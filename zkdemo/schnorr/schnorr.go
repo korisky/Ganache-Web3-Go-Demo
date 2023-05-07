8a986c90a8eb24c511b50a10bd2d4e8a0fca9bb0
roylic <lykismekk@outlook.com>
2023-05-07 22:23:57 +0800

    adding comments

--- zkdemo/schnorr/schnorr.go
+++ zkdemo/schnorr/schnorr.go
@@ -35,6 +35,8 @@ func SignSchnorr(curve elliptic.Curve, privateKey []byte, msg []byte) (r, s *big
 
 		// Calculate the modular inverse of k
 		kInv := new(big.Int).ModInverse(k, curve.Params().N)
+
+		// Compute s = (privateKey * r + e) * k^-1 mod n
 		temp := new(big.Int).Mul(new(big.Int).SetBytes(privateKey), r)
 		temp.Add(temp, e)
 		s = temp.Mul(temp, kInv)
@@ -50,22 +52,35 @@ func SignSchnorr(curve elliptic.Curve, privateKey []byte, msg []byte) (r, s *big
 // VerifySchnorr is for verifying
 func VerifySchnorr(curve elliptic.Curve, publicKeyX, publicKeyY *big.Int, msg []byte, r, s *big.Int) bool {
 
+	// Check if r and s are in the valid range [1, n-1]
 	if r.Cmp(curve.Params().N) >= 0 || s.Cmp(curve.Params().N) >= 0 {
 		return false
 	}
 
+	// Hash the message to create a fixed-length digest
 	hash := sha256.Sum256(msg)
 	e := new(big.Int).SetBytes(hash[:])
 
+	// Compute w = s^-1 mod n
 	w := new(big.Int).ModInverse(s, curve.Params().N)
+
+	// Compute u1 = e * w mod n
 	u1 := new(big.Int).Mul(e, w)
 	u1.Mod(u1, curve.Params().N)
+
+	// Compute u2 = r * w mod n
 	u2 := new(big.Int).Mul(r, w)
 	u2.Mod(u2, curve.Params().N)
 
+	// Calculate the point (x1, y1) = u1 * G, where G is the base point of the curve
 	x1, y1 := curve.ScalarBaseMult(u1.Bytes())
+
+	// Calculate the point (x2, y2) = u2 * (publicKeyX, publicKeyY)
 	x2, y2 := curve.ScalarMult(publicKeyX, publicKeyY, u2.Bytes())
+
+	// Calculate the point (x3, y3) = (x1, y1) + (x2, y2)
 	x3, _ := curve.Add(x1, y1, x2, y2)
 
+	// The signature is valid if r is equal to x3
 	return r.Cmp(x3) == 0
 }
