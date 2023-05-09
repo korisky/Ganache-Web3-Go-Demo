b24a452aa88c08f3aa7e65804d2c15b2e6bb6082
roylic <lykismekk@outlook.com>
2023-05-09 23:30:19 +0800

    tmp commit for correcting the transaction input amount coding

--- solana/portto/transferRelated_test.go
+++ solana/portto/transferRelated_test.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/binary"
 	"github.com/davecgh/go-spew/spew"
-	"math"
+	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/common"
+	"github.com/portto/solana-go-sdk/types"
 	"testing"
 	"web3Demo/portto/transfer"
 )
@@ -18,6 +20,34 @@ func Test_TransferSol(t *testing.T) {
 	}
 }
 
+func decodeTxnInput(transaction *client.GetTransactionResponse) (uint64, int) {
+
+	message := transaction.Transaction.Message
+	accounts := message.Accounts
+
+	// only one instruction -> must be system program related SOL transfer
+	if len(message.Instructions) == 1 {
+		data := message.Instructions[0].Data
+		return binary.LittleEndian.Uint64(data[4:]), 9
+	}
+
+	// filter for all tokenProgram related instructions
+	tokenProgramInstructions := make([]types.CompiledInstruction, len(message.Instructions))
+	for _, instruction := range message.Instructions {
+		if accounts[instruction.ProgramIDIndex] == common.TokenProgramID {
+			tokenProgramInstructions = append(tokenProgramInstructions, instruction)
+		}
+	}
+
+	// only one token program instruction -> must be normal SPL transfer
+	if len(tokenProgramInstructions) == 1 {
+		data := tokenProgramInstructions[0].Data
+		return binary.LittleEndian.Uint64(data[1:]), 9
+	}
+
+	return 0, 0
+}
+
 // Test_DecodingAmount
 func Test_DecodingAmount(t *testing.T) {
 
@@ -27,43 +57,44 @@ func Test_DecodingAmount(t *testing.T) {
 	transaction, _ := cli.GetTransaction(context.Background(), "5T2KkZ9fH9J1qbeTS1MhXVYjDfXoGVrAJrYrVEPmrLoSiUzn6xqGS264tphX6xCnN8zWF4ZrRkBphGfimExpEPQd") // spl-nft
 	spew.Dump(transaction)
 
-	for _, instruction := range transaction.Transaction.Message.Instructions {
-		amount, err := transfer.DecodeNativeTransferAmount(instruction)
-		if nil == err {
-
-			var realAmount float64
-			if instruction.ProgramIDIndex == 1 {
-				// for SOL transfer
-				realAmount = float64(amount) / math.Pow10(9)
-
-			} else if instruction.ProgramIDIndex == 4 {
-
-				// for SPL transfer
-				preTokenBalances := transaction.Meta.PreTokenBalances
-				postTokenBalances := transaction.Meta.PostTokenBalances
-
-				var decimal uint8
-				for _, balance := range preTokenBalances {
-					if balance.Owner == "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D" {
-						decimal = balance.UITokenAmount.Decimals
-						break
-					}
-				}
-				if 0 == decimal {
-					for _, balance := range postTokenBalances {
-						if balance.Owner == "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D" {
-							decimal = balance.UITokenAmount.Decimals
-							break
-						}
-					}
-				}
-
-				if 0 != decimal {
-					realAmount = float64(amount) / math.Pow10(int(decimal))
-				}
-			}
-
-			fmt.Printf("Decode transaction with amount: %v \n", realAmount)
-		}
-	}
+	//
+	//for _, instruction := range transaction.Transaction.Message.Instructions {
+	//	amount, err := transfer.DecodeNativeTransferAmount(instruction)
+	//	if nil == err {
+	//
+	//		var realAmount float64
+	//		if instruction.ProgramIDIndex == 1 {
+	//			// for SOL transfer
+	//			realAmount = float64(amount) / math.Pow10(9)
+	//
+	//		} else if instruction.ProgramIDIndex == 4 {
+	//
+	//			// for SPL transfer
+	//			preTokenBalances := transaction.Meta.PreTokenBalances
+	//			postTokenBalances := transaction.Meta.PostTokenBalances
+	//
+	//			var decimal uint8
+	//			for _, balance := range preTokenBalances {
+	//				if balance.Owner == "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D" {
+	//					decimal = balance.UITokenAmount.Decimals
+	//					break
+	//				}
+	//			}
+	//			if 0 == decimal {
+	//				for _, balance := range postTokenBalances {
+	//					if balance.Owner == "AnayTW335MabjhtXTJeBit5jdLhNeUVBVPXeRKCid79D" {
+	//						decimal = balance.UITokenAmount.Decimals
+	//						break
+	//					}
+	//				}
+	//			}
+	//
+	//			if 0 != decimal {
+	//				realAmount = float64(amount) / math.Pow10(int(decimal))
+	//			}
+	//		}
+	//
+	//		fmt.Printf("Decode transaction with amount: %v \n", realAmount)
+	//	}
+	//}
 }
