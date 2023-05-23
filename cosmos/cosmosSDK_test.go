ca6b7f1b278785f5800b59f2513a44558d1618a2
roylic <roylic@wokoworks.com>
2023-05-23 14:27:59 +0800

    add whole cosmos sdk's protobuf stuff

--- cosmos/cosmosSDK_test.go
+++ cosmos/cosmosSDK_test.go
@@ -1,37 +1,30 @@
 package cosmos
 
 import (
-	"context"
 	"fmt"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	"google.golang.org/grpc"
+	"io"
+	"net/http"
 
 	"testing"
 )
 
-func Test_queryState(t *testing.T) {
+func Test_getStatus(t *testing.T) {
 
-	address, _ := sdk.AccAddressFromHexUnsafe("cosmos1wrljuqeelfuksll8clpqz5nxsqhdr7jcp0psv9")
+	// Osmosis Testnet Rpc
+	rpcUrl := "https://rpc.osmotest5.osmosis.zone"
 
-	// Create a connection to the gRPC server.
-	grpcConn, _ := grpc.Dial(
-		"127.0.0.1:9090",    // your gRPC server address.
-		grpc.WithInsecure(), // The Cosmos SDK doesn't support any transport security mechanism.
-		// This instantiates a general gRPC codec which handles proto bytes. We pass in a nil interface registry
-		// if the request/response types contain interface instead of 'nil' you should pass the application specific codec.
-		grpc.WithDefaultCallOptions(grpc.ForceCodec(codec.NewProtoCodec(nil).GRPCCodec())),
-	)
-	defer grpcConn.Close()
+	resp, err := http.Get(rpcUrl + "/status")
+	if err != nil {
+		panic(err)
+	}
 
-	// This creates a gRPC client to query the x/bank service.
-	bankClient := banktypes.NewQueryClient(grpcConn)
-	bankRes, _ := bankClient.Balance(
-		context.Background(),
-		&banktypes.QueryBalanceRequest{Address: address.String(), Denom: "atom"},
-	)
+	defer resp.Body.Close()
 
-	fmt.Println(bankRes.GetBalance()) // Prints the account balance
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(body)
 
 }
