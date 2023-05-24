fed8a29565f01363d0a8e654870fe8d90402f7c6
roylic <roylic@wokoworks.com>
2023-05-24 17:06:48 +0800

    find out only osmosis accept github.com/cosmos/cosmos-sdk v0.47.2

--- cosmos/fxConnection_test.go
+++ cosmos/fxConnection_test.go
@@ -2,41 +2,20 @@ package cosmos
 
 import (
 	"context"
-	"fmt"
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"io"
 	"log"
-	"net/http"
 	"testing"
 )
 
-func Test_getStatusRpc(t *testing.T) {
+func Test_getLatestBlockHeight(t *testing.T) {
 
-	// Osmosis Testnet Rpc
-	rpcUrl := "https://rpc.osmotest5.osmosis.zone"
-
-	resp, err := http.Get(rpcUrl + "/status")
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(body)
-}
-
-func Test_getStatusGRpc(t *testing.T) {
-
-	//rpcUrl := "grpc.osmosis.zone:9090"
-	rpcUrl := "fx-grpc.functionx.io:9090"
+	//rpcUrl := "grpc.osmosis.zone:9090" // osmosis 获取成功
+	rpcUrl := "grpc.cosmos.interbloc.org:443" // cosmos-hub
+	//rpcUrl := "fx-grpc.functionx.io:9090" // fxcore 获取失败
+	//rpcUrl := "192.168.20.222:26657" // fxcore本地 获取失败
 
 	conn, err := grpc.Dial(rpcUrl,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
@@ -48,9 +27,7 @@ func Test_getStatusGRpc(t *testing.T) {
 	defer conn.Close()
 
 	tmClient := tmservice.NewServiceClient(conn)
-
 	request := tmservice.GetLatestBlockRequest{}
-
 	res, err := tmClient.GetLatestBlock(context.Background(), &request)
 	if err != nil {
 		log.Fatalf("Failed to get the latest block: %v", err)
