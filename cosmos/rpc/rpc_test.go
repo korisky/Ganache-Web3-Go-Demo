d4d4ee7d34d9f46a51d72eee6e45544711010fcb
roylic <roylic@wokoworks.com>
2023-05-26 14:03:46 +0800

    get latest block test

--- cosmos/rpc/rpc_test.go
+++ cosmos/rpc/rpc_test.go
@@ -3,18 +3,26 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/tendermint/tendermint/rpc/client/http"
 	cosmos "own.cosmos.demo"
 	"testing"
 )
 
-func Test_getStatus_Rpc(t *testing.T) {
+func Test_getLatestBlockHeight_Rpc(t *testing.T) {
+	// request
+	c, _ := http.New(cosmos.RpcUrl)
+	// decoding
+	resp, _ := c.Status(context.Background())
+	fmt.Println(resp.SyncInfo.LatestBlockHeight)
+}
+
+func Test_getLatestBlock_Rpc(t *testing.T) {
 
 	// request
 	c, _ := http.New(cosmos.RpcUrl)
-
+	blockHeight := int64(848636)
 	// decoding
-	resp, _ := c.Status(context.Background())
-
-	fmt.Println(resp.SyncInfo.LatestBlockHeight)
+	block, _ := c.Block(context.Background(), &blockHeight)
+	spew.Dump(block)
 }
