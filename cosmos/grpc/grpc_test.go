a03a8f81dc848c94af1b7d5e9ccdd2e6596e44c0
roylic <lykismekk@outlook.com>
2023-05-28 23:38:04 +0800

    adding node info request

--- cosmos/grpc/grpc_test.go
+++ cosmos/grpc/grpc_test.go
@@ -17,3 +17,14 @@ func Test_getLatestBlock(t *testing.T) {
 	res, _ := tmClient.GetLatestBlock(context.Background(), &request)
 	log.Println("Latest block height:", res.Block.Header.Height)
 }
+
+func Test_getNodeInfo(t *testing.T) {
+	defer cosmos.Conn.Close()
+
+	client := tmservice.NewServiceClient(cosmos.Conn)
+	request := tmservice.GetNodeInfoRequest{}
+	info, _ := client.GetNodeInfo(context.Background(), &request)
+	log.Printf("Node info: Network: %v, ListenAddr: %v", info.DefaultNodeInfo.Network, info.DefaultNodeInfo.ListenAddr)
+	log.Printf("Application info: goVer: %v, appName: %v, cosmosSdkVer: %v",
+		info.ApplicationVersion.GoVersion, info.ApplicationVersion.AppName, info.ApplicationVersion.CosmosSdkVersion)
+}
