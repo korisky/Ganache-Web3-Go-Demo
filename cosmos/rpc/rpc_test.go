d7c1858848d2fb6090db392ab9408b49badd23af
roylic <roylic@wokoworks.com>
2023-05-29 16:17:15 +0800

    adding block decoding test for rpc request

--- cosmos/rpc/rpc_test.go
+++ cosmos/rpc/rpc_test.go
@@ -3,7 +3,9 @@ package rpc
 import (
 	"context"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/types/tx"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/golang/protobuf/proto"
 	"github.com/tendermint/tendermint/rpc/client/http"
 	cosmos "own.cosmos.demo"
 	"testing"
@@ -17,12 +19,20 @@ func Test_getLatestBlockHeight_Rpc(t *testing.T) {
 	fmt.Println(resp.SyncInfo.LatestBlockHeight)
 }
 
-func Test_getLatestBlock_Rpc(t *testing.T) {
-
+func Test_decodeBlock_Rpc(t *testing.T) {
 	// request
 	c, _ := http.New(cosmos.RpcUrl)
 	blockHeight := int64(848636)
 	// decoding
 	block, _ := c.Block(context.Background(), &blockHeight)
-	spew.Dump(block)
+	// unmarshal all proto tx
+	for _, txBytes := range block.Block.Data.Txs {
+		var tx tx.Tx
+		err := proto.Unmarshal(txBytes, &tx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		spew.Dump(tx)
+	}
+
 }
