24c06b3d984b3412f4495e7b52b0dc635a2e5c13
roylic <lykismekk@outlook.com>
2023-06-08 23:32:42 +0800

    adding span coding

--- oTel/app.go
+++ oTel/app.go
@@ -3,9 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"io"
 	"log"
+	"strconv"
 )
+
+const name = "fib"
 
 // App is a Fibonacci computation application
 type App struct {
@@ -21,25 +27,53 @@ func NewApp(r io.Reader, l *log.Logger) *App {
 // Run stats polling users for Fibonacci number requests and writes results
 func (a *App) Run(ctx context.Context) error {
 	for {
+
+		// Each execution of the run loop, we should get a new "root" span and context
+		newCtx, span := otel.Tracer(name).Start(ctx, "Run")
+
 		n, err := a.Poll(ctx)
 		if err != nil {
+			span.End()
 			return err
 		}
-		a.Write(ctx, n)
+
+		a.Write(newCtx, n)
+		span.End()
 	}
 }
 
 // Poll asks a user for input and returns the request
 func (a *App) Poll(ctx context.Context) (uint, error) {
+
+	// adding tracer
+	_, span := otel.Tracer(name).Start(ctx, "Poll")
+	defer span.End()
+
 	a.l.Print("What Fibonacci number would you like to know: ")
 	var n uint
 	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+
+	// store n as a string to not overflow an int64
+	nStr := strconv.FormatUint(uint64(n), 10)
+	span.SetAttributes(attribute.String("request.n", nStr))
+
 	return n, err
 }
 
 // Write writes the n-th Fibonacci number back to the user
 func (a *App) Write(ctx context.Context, n uint) {
-	f, err := Fibbonacci(n)
+
+	// adding tracer
+	var span trace.Span
+	ctx, span = otel.Tracer(name).Start(ctx, "Write")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (uint64, error) {
+		ctx, span := otel.Tracer(name).Start(ctx, "Fibonacci")
+		defer span.End()
+		return Fibbonacci(n)
+	}(ctx)
+
 	if err != nil {
 		a.l.Printf("Fibonacci(%d): %v\n", n, err)
 	} else {
