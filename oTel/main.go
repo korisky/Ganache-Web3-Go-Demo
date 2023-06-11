68ada0c330ab65f629978a7c40bdd268a3874465
roylic <lykismekk@outlook.com>
2023-06-11 14:22:46 +0800

    adding traceProvider

--- oTel/main.go
+++ oTel/main.go
@@ -2,14 +2,46 @@ package main
 
 import (
 	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"io"
 	"log"
 	"os"
 	"os/signal"
 )
 
 func main() {
+
+	// for otel setting
 	l := log.New(os.Stdout, "", 0)
 
+	// write telemetry data to a file
+	f, err := os.Create("traces.txt")
+	if err != nil {
+		l.Fatal(err)
+	}
+	defer f.Close()
+
+	exp, err := newExporter(f)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(newResource()))
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal(err)
+		}
+	}()
+	otel.SetTracerProvider(tp)
+
+	// for application
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
 
@@ -28,4 +60,21 @@ func main() {
 			l.Fatal(err)
 		}
 	}
+}
+
+// newExporter returns a console exporter
+func newExporter(w io.Writer) (trace.SpanExporter, error) {
+	return stdouttrace.New(stdouttrace.WithWriter(w), stdouttrace.WithPrettyPrint(), stdouttrace.WithoutTimestamps())
+}
+
+// newResource returns a resource describing this application
+func newResource() *resource.Resource {
+	merge, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("fib"),
+			semconv.ServiceVersion("v0.1.0"),
+			attribute.String("environment", "demo")))
+	return merge
 }
