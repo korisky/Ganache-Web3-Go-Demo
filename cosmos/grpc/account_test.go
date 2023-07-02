7a3236c9747be35a7f9a848fce1475f2924e7525
roylic <lykismekk@outlook.com>
2023-07-02 17:36:12 +0800

    try create account by using keyring

--- cosmos/grpc/account_test.go
+++ cosmos/grpc/account_test.go
@@ -1,55 +1,26 @@
 package grpc
 
 import (
-	"encoding/hex"
-	"fmt"
-	"github.com/cosmos/btcutil/bech32"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/go-bip39"
-	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"log"
+
 	"testing"
 )
 
-func Test_AccountAgain(t *testing.T) {
-	fromBech32, _ := sdk.AccAddressFromBech32("osmo1xq9dsa599jurv2c8kj06duussplu9qmz0cxzru")
-	fmt.Println(fromBech32)
-}
+func Test_AccountGenerate(t *testing.T) {
 
-// Test_AccountRecover try recover account from mnemonic (not working, much more longer)
-func Test_AccountRecover(t *testing.T) {
+	registry := codectypes.NewInterfaceRegistry()
+	cryptocodec.RegisterInterfaces(registry)
+	cdc := codec.NewProtoCodec(registry)
 
-	// Replace this with your Mnemonic Phrase
-	mnemonic := "window turn course input bag clog elbow witness globe switch latin gasp"
-
-	// Generating the seed from Mnemonic
-	seed := bip39.NewSeed(mnemonic, "Thunder123LYK")
-
-	// Deriving the private key
-	master, ch := hd.ComputeMastersFromSeed(seed)
-	priv, err := hd.DerivePrivateKeyForPath(master, ch, "m/44'/118'/0'/0/0'")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Generating the public key
-	privKey := secp256k1.GenPrivKeySecp256k1(priv)
-	pubKey := privKey.PubKey()
-
-	// Generating the address
-	decodeString, _ := hex.DecodeString(fmt.Sprintf("04%x", pubKey.Bytes()))
-	conv, err := bech32.ConvertBits(decodeString, 8, 5, true)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	// osmo1xq9dsa599jurv2c8kj06duussplu9qmz0cxzru
-	encoded, err := bech32.Encode("osmo", conv)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
-	// Printing the address
-	fmt.Println("Atom address:", encoded)
+	kb := keyring.NewInMemory(cdc)
+	record, _ := kb.NewAccount(
+		"some_account",
+		"malarkey pair crucial catch public canyon evil outer stage ten gym tornado",
+		"", "", hd.Secp256k1)
+	log.Println(record.PubKey)
 }
