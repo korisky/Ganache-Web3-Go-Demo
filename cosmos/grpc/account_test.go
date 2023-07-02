1032e3f8a9486385741106bfea9d76555da8fe07
roylic <lykismekk@outlook.com>
2023-07-02 23:25:26 +0800

    finally could successfully recover Cosmos Account from mnemonic & get correct bech32ify address

--- cosmos/grpc/account_test.go
+++ cosmos/grpc/account_test.go
@@ -1,26 +1,55 @@
 package grpc
 
 import (
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/golang/protobuf/proto"
 	"log"
 
 	"testing"
 )
 
-func Test_AccountGenerate(t *testing.T) {
+var mnemonic = ""
 
+// Test_RecoverAccountByMnemonic recover account from mnemonic -> keyring -> account could divide public address
+func Test_RecoverAccountByMnemonic(*testing.T) {
+
+	// create codec
 	registry := codectypes.NewInterfaceRegistry()
 	cryptocodec.RegisterInterfaces(registry)
 	cdc := codec.NewProtoCodec(registry)
 
+	// recover account by using mnemonic
 	kb := keyring.NewInMemory(cdc)
-	record, _ := kb.NewAccount(
-		"some_account",
-		"malarkey pair crucial catch public canyon evil outer stage ten gym tornado",
-		"", "", hd.Secp256k1)
-	log.Println(record.PubKey)
+	record, _ := kb.NewAccount("osmo", mnemonic, keyring.DefaultBIP39Passphrase, sdk.FullFundraiserPath, hd.Secp256k1)
+
+	// Set the config for the Cosmos SDK.
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount("osmo", "osmo")
+
+	// Extract the public key bytes from the record.
+	pubKeyBytes := record.PubKey.Value
+
+	// Unmarshal the bytes into the secp256k1.PubKey structure.
+	var pubKey secp256k1.PubKey
+	if err := proto.Unmarshal(pubKeyBytes, &pubKey); err != nil {
+		log.Fatalf("Error unmarshalling public key: %v", err)
+	}
+
+	// Convert the secp256k1 public key to a Cosmos SDK AccAddress.
+	addressBytes := sdk.AccAddress(pubKey.Address().Bytes())
+
+	// Convert the AccAddress bytes to a Bech32 encoded string.
+	addressString, err := sdk.Bech32ifyAddressBytes(config.GetBech32AccountAddrPrefix(), addressBytes)
+	if err != nil {
+		log.Fatalf("Error converting address bytes to bech32: %v", err)
+	}
+
+	fmt.Printf("Cosmos address (Bech32): %s\n", addressString)
 }
