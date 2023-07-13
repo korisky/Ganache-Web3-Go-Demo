345f28087ff170293efad85285ab593b278aa4e9
roylic <roylic@wokoworks.com>
2023-07-13 13:05:00 +0800

    add fx endpoint

--- goerli/quickNode.go
+++ goerli/quickNode.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"own.goerli.demo/event"
+	"own.goerli.demo/transaction"
 )
 
 var (
 	ctx = context.Background()
-	//url = "https://goerli.infura.io/v3/3f0482cf4c3545dbabaeab75f414e467"
-	url = ""
+	url = "https://goerli.infura.io/v3/3f0482cf4c3545dbabaeab75f414e467"
+	//url = "https://testnet-fx-json-web3.functionx.io:8545"
 	// connect to own web3 node url
 	client, _ = ethclient.DialContext(ctx, url)
 )
@@ -32,9 +32,10 @@ func main() {
 	//account.JudgeAddressOrSmartContract(client, "0xe10eE98bB84B2073B88353e3AB4433916205DF40")
 	//account.JudgeAddressOrSmartContract(client, "0xff02b7d59975E76F67B63b20b813a9Ec0f6AbD60")
 	//
-	//// send raw transfer
-	//transfer.SendingRawTransfer(ctx, client)
 
-	event.SubscribeErc1155SingleTransferEvent(ctx, client)
+	// send raw transfer
+	transaction.SendingRawTransfer(ctx, client)
+
+	//event.SubscribeErc1155SingleTransferEvent(ctx, client)
 
 }
