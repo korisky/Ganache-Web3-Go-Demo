78eabb80004727ac5120b0bede333fcddc79eba4
Roylic <35546172+korisky@users.noreply.github.com>
2025-05-05 15:36:28 +0800

    chore: add .gitignore & detailed comments

--- sys_chan/main.go
+++ sys_chan/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+
+	// buffered channel with 1 capacity is usually a good choice
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
 	fmt.Println("Server running...")
