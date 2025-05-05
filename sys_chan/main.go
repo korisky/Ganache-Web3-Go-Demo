a56b6dc6c7855f79879623a1e419ff7b3d5f51ba
Roylic <35546172+korisky@users.noreply.github.com>
2025-05-05 16:04:00 +0800

    feat: add signal handling which binding with ctx, better for web-app

--- sys_chan/main.go
+++ sys_chan/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -9,19 +10,35 @@ import (
 
 func main() {
 
-	// before go 1.16
-	// buffered channel with 1 capacity is usually a good choice
-	PureIntTerSigHandling()
+	// Before go 1.16
+	//PureIntTerSigHandling()
 
-	// after go 1.16, ties the signal handling with Ctx
+	// After go 1.16, ties the signal handling with Ctx
+	BindIntTerSigHandlingWithCtx()
 
+}
+
+// BindIntTerSigHandlingWithCtx after go ver 1.16, OS signal handling could bind with ctx
+func BindIntTerSigHandlingWithCtx() {
+	// bind notify with context
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	fmt.Println("Server running...")
+
+	// tasks here
+	<-ctx.Done()
+	fmt.Println("\nReceived termination signal, shutting down...")
+	stop()
 }
 
 // PureIntTerSigHandling before go ver 1.16, purely handle SIG_INT & SIG_TERM
 func PureIntTerSigHandling() {
+	// buffered channel with 1 capacity is usually a good choice
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
 	fmt.Println("Server running...")
+
+	// tasks here
 
 	// hangUp waiting for interrupt / termination OS signal
 	<-signalChan
