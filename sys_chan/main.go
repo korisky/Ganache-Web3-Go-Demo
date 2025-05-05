f72b50570dbff0b44d0d300e8901a6683971bd73
Roylic <35546172+korisky@users.noreply.github.com>
2025-05-05 15:41:30 +0800

    refactor: signal handling extraction

--- sys_chan/main.go
+++ sys_chan/main.go
@@ -9,12 +9,24 @@ import (
 
 func main() {
 
+	// before go 1.16
 	// buffered channel with 1 capacity is usually a good choice
+	PureIntTerSigHandling()
+
+	// after go 1.16, ties the signal handling with Ctx
+
+}
+
+// PureIntTerSigHandling before go ver 1.16, purely handle SIG_INT & SIG_TERM
+func PureIntTerSigHandling() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT)
 	fmt.Println("Server running...")
 
 	// hangUp waiting for interrupt / termination OS signal
 	<-signalChan
+
+	// for handling second Ctrl+C
+	signal.Stop(signalChan)
 	fmt.Println("\nReceived termination signal, shutting down...")
 }
